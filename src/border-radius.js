d66aa8de31dd0cbfe04e5b9546aba1fccfa5bf11
Yuri Tkachenko <yuri@ya.ru>
2016-11-27 13:26:32 +0200

    lint source

--- src/border-radius.js
+++ src/border-radius.js
@@ -1,29 +1,28 @@
+import {mapValues, some} from 'lodash'
 import {camelCaseKeys, joinDimensionPairs} from './utils'
-import {mapValues, some, uniq} from 'lodash'
 
 export default function borderRadius(conf) {
-    const props = mapValues(
-        camelCaseKeys(conf),
-        value => value.split('/').map(value => value.trim())
-    )
-    const {
-        borderTopLeftRadius = [0],
-        borderTopRightRadius = [0],
-        borderBottomRightRadius = [0],
-        borderBottomLeftRadius = [0]
-    } = props
+  const props = mapValues(
+      camelCaseKeys(conf),
+      fullValue => fullValue.split('/').map(value => value.trim())
+  )
+  const {
+      borderTopLeftRadius = [0],
+      borderTopRightRadius = [0],
+      borderBottomRightRadius = [0],
+      borderBottomLeftRadius = [0]
+  } = props
 
-    const hasPercentage = some(props, value => value[1])
-    const values = [
-        borderTopLeftRadius,
-        borderTopRightRadius,
-        borderBottomRightRadius,
-        borderBottomLeftRadius
-    ]
-
-    const lengthsStr = joinDimensionPairs(values.map(value => value[0]))
-    const percentagesStr = hasPercentage ? joinDimensionPairs(values.map(value => value[1] || value[0])) : false
-    return {
-        'border-radius': lengthsStr + (percentagesStr ? ` / ${percentagesStr}` : '')
-    }
+  const hasPercentage = some(props, value => value[1])
+  const values = [
+    borderTopLeftRadius,
+    borderTopRightRadius,
+    borderBottomRightRadius,
+    borderBottomLeftRadius
+  ]
+  const length = joinDimensionPairs(values.map(value => value[0]))
+  const percentage = hasPercentage && joinDimensionPairs(values.map(value => value[1] || value[0]))
+  return {
+    'border-radius': length + (percentage ? ` / ${percentage}` : '')
+  }
 }
--- src/utils.js
+++ src/utils.js
@@ -1,14 +1,15 @@
 import {mapKeys, camelCase} from 'lodash'
 
 export function camelCaseKeys(obj) {
-    return mapKeys(obj, (key, value) => camelCase(value))
+  return mapKeys(obj, (key, value) => camelCase(value))
 }
 
 export function joinDimensionPairs(values) {
-    let [a, b, c, d] = values
-    if (a === c) c = ''
-    if (b === d) d = ''
-    if (c === '' && d === '' && a === b) b = ''
+  const [a] = values
+  let [, b, c, d] = values
+  if (a === c) c = ''
+  if (b === d) d = ''
+  if (c === '' && d === '' && a === b) b = ''
 
-    return [a, b, c, d].filter(value => value !== '').join(' ')
+  return [a, b, c, d].filter(value => value !== '').join(' ')
 }
