0cfca567348cab3a690136047f2aa8e415ee4646
Yuri Tkachenko <yuri@ya.ru>
2016-11-29 13:14:23 +0200

    lint tets

--- test/init.js
+++ test/init.js
@@ -1,10 +1,6 @@
 import 'babel-polyfill'
 import chai from 'chai'
+
 global.expect = chai.expect
-global.log = (s) => {
-    console.log('====')
-    console.log(s)
-    console.log('====')
-}
 chai.should()
 
